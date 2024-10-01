795a5b0af39c990459a10f2d29b88addaa3fd927
kalyan <kalyan.raparthi.com>
2024-10-02 00:12:21 +0530

    many

--- rust/ser/src/main.rs
+++ rust/ser/src/main.rs
@@ -2,12 +2,19 @@ pub mod out;
 use out::print_buf;
 
 use std::net::TcpListener;
-use std::io::{Read, Write, stdin};
+use std::io::{stdin, BufWriter, Read, Write};
 
 fn main() {
-    let mut buf = [0; 512];
-    let l1 = TcpListener::bind("127.0.0.1:2024").unwrap();
-    
+    println!("\nserver\n");
+
+    let mut buf:[u8; 512] = [2; 512];
+
+    let l1 = TcpListener::bind("127.0.0.1:2024").unwrap(); 
     let (mut stream, addr) = l1.accept().unwrap();
     println!("connected : {}", addr);
+    
+    stream.write(&mut buf).unwrap();
+
+    stream.read(&mut buf).unwrap();
+    println!("{:?}", buf);
 }
